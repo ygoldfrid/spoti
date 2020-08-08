23ee5ccc59d04dcdd8f78027ece7d02032a34b02
ygoldfrid <ygoldfrid@gmail.com>
2020-08-08 13:22:13 -0500

    Vercel Config added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "./index.css";
 
-document.title = process.env.react_app_name;
+document.title = process.env.REACT_APP_NAME;
 
 logger.init();
 spotiPlayer.init();
